c1b13dce6e2fc364838e6fb0a28ccd81ff9d1a5d
alanzeng1213 <35350269+alanzeng1213@users.noreply.github.com>
2018-12-21 11:35:38 -0500

    Update Test.java

--- src/com/alan/Test.java
+++ src/com/alan/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("left side....");
 		
-		System.out.println("this is left side new branch..");
+		System.out.println("this is left side new branch2..");
 	}
 }
